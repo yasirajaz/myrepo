baa7cac8a6f2b65655d7db8656cd5fe62a92328f
YAX1RRR <yax1rrr.pvt@gmail.com>
2025-05-17 14:59:45 +0530

    17 may

---
+++ alien_invasion/settings.py
@@ -0,0 +1,14 @@
+class Settings:
+	def __init__(self):
+		#ship ki settings
+		self.screen_width=1200
+		self.screen_height=800
+		self.bg_color=(230,230,230)
+		self.ship_speed=0.5
+
+		#bullet settings
+		self.bullet_speed = 0.5
+		self.bullet_width = 5
+		self.bullet_height = 20
+		self.bullet_color = (0, 64, 26)
+		self.bullets_allowed = 3
