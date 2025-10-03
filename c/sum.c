fced7e190caf4c2dea2fbeec393cf29812d385e5
YAX1RRR <yax1rrr.pvt@gmail.com>
2025-10-03 18:09:58 +0530

    03 october  2025

---
+++ c/sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+        int x,y;
+        printf("enter two number: ");
+        scanf("%d %d",&x,&y);
+        int add= x+y;
+        printf("sum= %d\n",add);
+        return 0;
+}
