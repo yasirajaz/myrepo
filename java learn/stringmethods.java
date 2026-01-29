7ac838f07edc58aed12688b85d29f7e9338c9d78
YAX1RRR <yax1rrr.pvt@gmail.com>
2026-01-29 17:39:10 +0530

    29 jan till java methods

--- java learn/stringmethods.java
+++ java learn/stringmethods.java
@@ -30,5 +30,19 @@ class stringmethods{
 		System.out.println(ch.length);
 		String s8= new String(ch);
 		System.out.println(s8);
+
+
+		String s9=s1.substring(0,4); // 0 se leke 4-1 tk
+		System.out.println(s9);
+		String s10=s1.substring(4,7); // 4 se leke 7-1 tk
+		System.out.println(s10);
+		String s11= s1.substring(2); // 2 se leke last tk
+		System.out.println(s11);
+
+		String s12="Lucknow-Kanpur-Banaras";
+		String[] sa=s12.split("-");
+		for(int i=0;i<sa.length;i++){
+			System.out.print(sa[i]+"  ");
+		}
 	}
 }
